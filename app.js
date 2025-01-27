7e4c842071dce0d5ad4df187f59379869dff039a
kabayakisan <24hs107@s.bgu.ac.jp>
2025-01-27 13:51:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -6,52 +6,111 @@ function checkFortune() {
      
     if (animal === "うさぎ" && number === 1) {
         result = "青";
-    } else if (animal  === "うさぎ" && number === 2) {
+        if (animal === "うさぎ" && number === 2) {
+        result = "ピンク";
+            if (animal === "うさぎ" && number === 3) {
+        result = "赤";
+    } else if (animal  === "うさぎ" && number === 4) {
+        result = "黒";
+                
+    } else if (animal  === "いぬ" && number === 1) {
         result = "青";
-    } else if (animal  === "いぬ" && number === 3) {
+     } else if (animal  === "いぬ" && number === 2) {
         result = "ピンク";
+     } else if (animal  === "いぬ" && number === 3) {
+        result = "赤";
     } else if (animal  === "いぬ" && number === 4) {
-        result = "ピンク";
+        result = "黒";
+                
     } else if (animal  === "ねこ" && number === 1) {
+        result = "青";
+ } else if (animal  === "ねこ" && number ===  2) {
+        result = "ピンク";
+     } else if (animal  === "ねこ" && number === 3) {
+        result = "赤";                
+    } else if (animal  === "ねこ" && number === 4) {
+        result = "黒";
+                
+    } else if (animal  === "くま" && number === 1) {
+        result = "青";
+            } else if (animal  === "くま" && number === 2) {
+        result = "ピンク";
+                 } else if (animal  === "くま" && number === 3) {
         result = "赤";
-    } else if (animal  === "ねこ" && number === 2) {
-        result = "赤";
-    } else if (animal  === "くま" && number === 3) {
-        result = "水色";
-    } else if (animal  === "くま" && number === 4) {
-        result = "水色";
+    } else if (animal  === "くま" && number === 4 ) {
+        result = "黒";
+                
     } else if (animal  === "ひつじ" && number === 1) {
+        result = "青";
+                  } else if (animal  === "ひつじ" && number === 　2) {
+        result = "ピンク";
+                  } else if (animal  === "ひつじ" && number === 3) {
+        result = "赤";
+    } else if (animal  === "ひつじ" && number === 4) {
         result = "黒";
-    } else if (animal  === "ひつじ" && number === 2) {
-        result = "黒";
-    } else if (animal  === "きりん" && number === 3) {
-        result = "オレンジ";
+                
+    } else if (animal  === "きりん" && number === 1) {
+        result = "青";
+                  } else if (animal  === "きりん" && number === 2) {
+        result = "ピンク";
+                  } else if (animal  === "きりん" && number === 3) {
+        result = "赤";
     } else if (animal  === "きりん" && number === 4) {
-        result = "オレンジ";
+        result = "黒";
+                
     } else if (animal  === "ぞう" && number === 1) {
-        result = "ゴールド";
-    } else if (animal  === "ぞう" && number === 2) {
-        result = "ゴールド";
-    } else if (animal  === "ぱんだ" && number === 3) {
-        result = "パープル";
+        result = "青";
+                } else if (animal  === "ぞう" && number === 2) {
+        result = "ピンク";
+                } else if (animal  === "ぞう" && number === 3) {
+        result = "赤";
+    } else if (animal  === "ぞう" && number === 4) {
+        result = "黒";
+                
+    } else if (animal  === "ぱんだ" && number === 1) {
+        result = "青";
+                  } else if (animal  === "ぱんだ" && number === 2) {
+        result = "ピンク";
+                  } else if (animal  === "ぱんだ" && number === 3) {
+        result = "赤";
     } else if (animal  === "ぱんだ" && number === 4) {
-        result = "パープル";
+        result = "黒";
+                
     } else if (animal  === "こあら" && number === 1) {
-        result = "黄色";
-    } else if (animal  === "こあら" && number === 2) {
-        result = "黄色";
-    } else if (animal  === "きつね" && number === 3) {
-        result = "白";
+        result = "青";
+                 } else if (animal  === "こあら" && number === 2) {
+        result = "ピンク";
+                 } else if (animal  === "こあら" && number === 3) {
+        result = "赤";
+    } else if (animal  === "こあら" && number === 4) {
+        result = "黒";
+                
+    } else if (animal  === "きつね" && number === 1) {
+        result = "青";
+                 } else if (animal  === "きつね" && number === 2) {
+        result = "ピンク";
+                 } else if (animal  === "きつね" && number === 3) {
+        result = "赤";
     } else if (animal  === "きつね" && number === 4) {
-        result = "白";
+        result = "黒";
+                
     } else if (animal  === "はりねずみ" && number === 1) {
-        result = "黄緑";
-    } else if (animal  === "はりねずみ" && number === 2) {
-        result = "黄緑";
-    } else if (animal  === "りす" && number === 3) {
-        result = "ベージュ";
+        result = "青";
+                 } else if (animal  === "はりねずみ" && number === 2) {
+        result = "ピンク";
+                 } else if (animal  === "はりねずみ" && number === 3) {
+        result = "赤";
+    } else if (animal  === "はりねずみ" && number === 4) {
+        result = "黒";
+                
+    } else if (animal  === "りす" && number === 1) {
+        result = "青";
+                } else if (animal  === "りす" && number === 2) {
+        result = "ピンク";
+                } else if (animal  === "りす" && number === 3) {
+        result = "赤";
     } else if (animal  === "りす" && number === 4) {
-        result = "ベージュ";
+        result = "黒";
     }
  
     
