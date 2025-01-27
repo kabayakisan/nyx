36a8923888ce697431a6d133c126bcc903bfb719
kabayakisan <24hs107@s.bgu.ac.jp>
2025-01-27 13:22:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,56 @@
 function checkFortune() {
-    var zodiacSigns = document.getElementById('zodiac-signs-select').value;
+    var animal = document.getElementById('animal-select').value;
     var number = parseInt(document.getElementById('number-input').value, 10);
     var result = "ラッキーカラー"; // デフォルトの値
  
      
-    if (zodiacSigns === "うお座" && number === 1) {
+    if (animal === "うさぎ" && number === 1) {
         result = "青";
-    } else if (zodiacSigns === "うお座" && number === 2) {
+    } else if (animal  === "うさぎ" && number === 2) {
         result = "青";
-    } else if (zodiacSigns === "みずがめ座" && number === 3) {
+    } else if (animal  === "いぬ" && number === 3) {
         result = "ピンク";
-    } else if (zodiacSigns === "みずがめ座" && number === 4) {
+    } else if (animal  === "いぬ" && number === 4) {
         result = "ピンク";
-    } else if (zodiacSigns === "おとめ座" && number === 1) {
+    } else if (animal  === "ねこ" && number === 1) {
         result = "赤";
-    } else if (zodiacSigns === "おとめ座" && number === 2) {
+    } else if (animal  === "ねこ" && number === 2) {
         result = "赤";
-    } else if (zodiacSigns === "かに座" && number === 3) {
+    } else if (animal  === "くま" && number === 3) {
         result = "水色";
-    } else if (zodiacSigns === "かに座" && number === 4) {
+    } else if (animal  === "くま" && number === 4) {
         result = "水色";
-    } else if (zodiacSigns === "おひつじ座" && number === 1) {
+    } else if (animal  === "ひつじ" && number === 1) {
         result = "黒";
-    } else if (zodiacSigns === "おひつじ座" && number === 2) {
+    } else if (animal  === "ひつじ" && number === 2) {
         result = "黒";
-    } else if (zodiacSigns === "ふたご座" && number === 3) {
+    } else if (animal  === "きりん" && number === 3) {
         result = "オレンジ";
-    } else if (zodiacSigns === "ふたご座" && number === 4) {
+    } else if (animal  === "きりん" && number === 4) {
         result = "オレンジ";
-    } else if (zodiacSigns === "てんびん座" && number === 1) {
+    } else if (animal  === "ぞう" && number === 1) {
         result = "ゴールド";
-    } else if (zodiacSigns === "てんびん座" && number === 2) {
+    } else if (animal  === "ぞう" && number === 2) {
         result = "ゴールド";
-    } else if (zodiacSigns === "さそり座" && number === 3) {
+    } else if (animal  === "ぱんだ" && number === 3) {
         result = "パープル";
-    } else if (zodiacSigns === "さそり座" && number === 4) {
+    } else if (animal  === "ぱんだ" && number === 4) {
         result = "パープル";
-    } else if (zodiacSigns === "しし座" && number === 1) {
+    } else if (animal  === "こあら" && number === 1) {
         result = "黄色";
-    } else if (zodiacSigns === "しし座" && number === 2) {
+    } else if (animal  === "こあら" && number === 2) {
         result = "黄色";
-    } else if (zodiacSigns === "おうし座" && number === 3) {
+    } else if (animal  === "きつね" && number === 3) {
         result = "白";
-    } else if (zodiacSigns === "おうし座" && number === 4) {
+    } else if (animal  === "きつね" && number === 4) {
         result = "白";
-    } else if (zodiacSigns === "いて座" && number === 1) {
+    } else if (animal  === "はりねずみ" && number === 1) {
         result = "黄緑";
-    } else if (zodiacSigns === "いて座" && number === 2) {
+    } else if (animal  === "はりねずみ" && number === 2) {
         result = "黄緑";
-    } else if (zodiacSigns === "やぎ座" && number === 3) {
+    } else if (animal  === "りす" && number === 3) {
         result = "ベージュ";
-    } else if (zodiacSigns === "やぎ座" && number === 4) {
+    } else if (animal  === "りす" && number === 4) {
         result = "ベージュ";
     }
  
